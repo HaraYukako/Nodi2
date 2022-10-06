ab0d75f6ac487ad0c0b81cb9cbd3d9d65341c3cf
原 夕香子 <HARAY1@tss.co.jp>
2022-10-06 09:50:59 +0900

    5

--- src/test/java/com/example/demo/ModelTest.java
+++ src/test/java/com/example/demo/ModelTest.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
